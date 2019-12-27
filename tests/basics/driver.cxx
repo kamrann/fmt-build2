9d35e4eb41c45903c6d202117584d5cc8d2283d2
A. Joël Lamotte (Klaim) <mjklaim@gmail.com>
2019-12-27 19:22:35 +0100

    Renamed to just "fmt"

--- tests/basics/driver.cxx
+++ tests/basics/driver.cxx
@@ -2,32 +2,12 @@
 #include <sstream>
 #include <stdexcept>
 
-#include <fmt-build2/version.hxx>
-#include <fmt-build2/fmt-build2.hxx>
+#include <fmt/version.h>
+#include <fmt/printf.h>
+#include <fmt/format.h>
 
 int main ()
 {
-  using namespace std;
-  using namespace fmt_build2;
-
-  // Basics.
-  //
-  {
-    ostringstream o;
-    say_hello (o, "World");
-    assert (o.str () == "Hello, World!\n");
-  }
-
-  // Empty name.
-  //
-  try
-  {
-    ostringstream o;
-    say_hello (o, "");
-    assert (false);
-  }
-  catch (const invalid_argument& e)
-  {
-    assert (e.what () == string ("empty name"));
-  }
+  ufmt::print("Hello, {}!", "world");  // Python-like format string syntax
+  fmt::printf("Hello, %s!", "world"); // printf format string syntax
 }
