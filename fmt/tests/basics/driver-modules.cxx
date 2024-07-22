a9ee48b20b5066150bd58eea4725a159eb1e7938
Cameron Angus <cjangus@gmail.com>
2024-07-22 19:40:32 +0100

    Add config option for a modules-only mode.

--- fmt/tests/basics/driver-modules.cxx
+++ fmt/tests/basics/driver-modules.cxx
@@ -1,4 +1,9 @@
 
+// Verify that fmt headers are not available if config.fmt.modules_only is true
+#if __has_include(<fmt/version.h>) != FMT_BUILD2_HAS_HEADER
+#error fmt headers should be available for include iff config.fmt.modules_only == false
+#endif
+
 #include <cassert>
 #include <string>
 #include <chrono>
@@ -7,5 +12,3 @@
 import fmt;
 
 #include "tests.inl"
-
-
